e8f75d8d0cdd0eec0b930099fc7d3cc023adb43c
Deepti Singh <deepti.singh_cs19@gla.ac.in>
2023-12-16 11:32:36 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import './style.scss';
 import Board from './Components/Board';
-import { useState } from 'react';
+// import { useState } from 'react';
 function App() {
-  const [counter, setCounter] = useState(1);
-  console.log('hello');
+  // const [counter, setCounter] = useState(1);
 
-  const onBtnClick = () => {
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
-  };
+  // const onBtnClick = () => {
+  //   setCounter(currentCounter => {
+  //     return currentCounter + 1;
+  //   });
+  // };
 
   return (
     <div className="app">
       <div>
-        <button onClick={onBtnClick}>Click me please</button>
-        <div>{counter}</div>
+        {/* <button onClick={onBtnClick}>Click me please</button> */}
+        {/* <div>{counter}</div> */}
       </div>
       <Board />
     </div>
