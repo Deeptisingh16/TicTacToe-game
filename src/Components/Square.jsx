48f5f53864ecda986b0c0ef3dcac55dde86eb997
Deepti Singh <deepti.singh_cs19@gla.ac.in>
2023-12-17 11:39:20 +0530

    highlight winner

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,15 +1,22 @@
 // eslint-disable-next-line react/prop-types
-const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
+  const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+  const winningClassName = isWinningSquare ? 'winning' : '';
   return (
-    <button type="button" className="square" onClick={onClick}>
+    <button
+      type="button"
+      className={`square ${colorClassName} ${winningClassName}`}
+      onClick={onClick}
+    >
       {value}
     </button>
   );
 };
+export default Square;
+
 //   console.log(props.value);
 //   return <div>{value}</div>;
 //   console.log(children);
 /* 
       <h5>render below:</h5>
       <div>{children}</div> */
-export default Square;
